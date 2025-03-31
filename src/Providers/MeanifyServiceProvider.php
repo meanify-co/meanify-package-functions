582b9deb910123b0570eca9d040b7913a2e49613
Daniel Barcelos <daniel.barcelos@2mind.com.br>
2025-03-31 17:24:51 -0300

    Update service provider to create class extended from request

--- src/Providers/MeanifyServiceProvider.php
+++ src/Providers/MeanifyServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Meanify\PackageFunctions\Providers;
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class MeanifyServiceProvider extends ServiceProvider
@@ -11,8 +12,28 @@ class MeanifyServiceProvider extends ServiceProvider
         //
     }
 
+    /**
+     * @return void
+     */
     public function boot(): void
     {
-        //
+        Request::macro('meanify', function () {
+            return new class {
+                public function headers(): \Illuminate\Support\Collection
+                {
+                    return collect(request()->headers->all())
+                        ->filter(fn ($_, $key) => str_starts_with($key, 'x-mfy-'))
+                        ->map(fn ($value) => $value[0] ?? null);
+                }
+                public function getHeader(string $key, mixed $default = null): mixed
+                {
+                    return request()->headers->get("x-mfy-{$key}", $default);
+                }
+                public function setHeader(string $key, mixed $value): void
+                {
+                    request()->headers->set("x-mfy-{$key}", $value);
+                }
+            };
+        });
     }
 }
